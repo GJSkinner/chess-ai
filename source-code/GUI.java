1a78554904ff5cccc01b82e5eeb3510cba7f95ca
George Skinner <56996684+GJSkinner@users.noreply.github.com>
2022-10-24 18:12:37 +0100

    improved visuals

--- source-code/GUI.java
+++ source-code/GUI.java
@@ -3,6 +3,13 @@ import java.awt.*;
 
 public class GUI extends JPanel {
 
+    public GUI() {
+
+        setOpaque(true);
+        setBackground(new Color(40, 40, 40));
+
+    }
+
     @Override
     public void paintComponent(Graphics gg) {
 
@@ -20,43 +27,40 @@ public class GUI extends JPanel {
                 g.drawRect(rowCount * 100 + 100, colCount * 100 + 100, 100, 100);
 
                 if (board[rowCount][colCount].getSelected()) {
-                    g.setColor(new Color(180, 180, 105));
+                    g.setColor(new Color(210, 210, 100));
                 } else {
                     if ((rowCount + colCount) % 2 == 0) {
-                        g.setColor(new Color(130, 180, 105));
+                        g.setColor(new Color(200, 230, 210));
                     } else {
-                        g.setColor(new Color(65, 75, 55));
+                        g.setColor(new Color(130, 180, 105));
                     }
                 }
 
                 g.fillRect(rowCount * 100 + 100, colCount * 100 + 100, 100, 100);
 
-                if (board[rowCount][colCount].getHighlighted()) {
-                    g.setColor(new Color(187, 201, 179));
-                    g.setFont(new Font(g.getFont().toString(), Font.PLAIN, 40));
-                    g.drawString("\u2726", rowCount * 100 + 133, colCount * 100 + 163);
-                }
-
-                if (board[rowCount][colCount].getIsWhite() == 1) {
-                    g.setColor(Color.white);
-                } else if (board[rowCount][colCount].getIsWhite() == 0) {
-                    g.setColor(Color.black);
-                }
+                boolean black = board[rowCount][colCount].getPiece() > 6;
 
                 g.setFont(new Font(g.getFont().toString(), Font.PLAIN, 80));
+                g.setColor(Color.black);
 
-                if (board[rowCount][colCount].getPiece() == 1) {
-                    g.drawString("\u2659", rowCount * 100 + 110, colCount * 100 + 180);
-                } else if (board[rowCount][colCount].getPiece() == 2) {
-                    g.drawString("\u2658", rowCount * 100 + 110, colCount * 100 + 180);
-                } else if (board[rowCount][colCount].getPiece() == 3) {
-                    g.drawString("\u2657", rowCount * 100 + 110, colCount * 100 + 180);
-                } else if (board[rowCount][colCount].getPiece() == 4) {
-                    g.drawString("\u2656", rowCount * 100 + 110, colCount * 100 + 180);
-                } else if (board[rowCount][colCount].getPiece() == 5) {
-                    g.drawString("\u2655", rowCount * 100 + 110, colCount * 100 + 180);
-                } else if (board[rowCount][colCount].getPiece() == 6) {
-                    g.drawString("\u2654", rowCount * 100 + 110, colCount * 100 + 180);
+                if (board[rowCount][colCount].getPiece() == 1 || board[rowCount][colCount].getPiece() == 7) {
+                    g.drawString((black) ? "\u265F" : "\u2659", rowCount * 100 + 110, colCount * 100 + 180);
+                } else if (board[rowCount][colCount].getPiece() == 2 || board[rowCount][colCount].getPiece() == 8) {
+                    g.drawString((black) ? "\u265E" : "\u2658", rowCount * 100 + 110, colCount * 100 + 180);
+                } else if (board[rowCount][colCount].getPiece() == 3 || board[rowCount][colCount].getPiece() == 9) {
+                    g.drawString((black) ? "\u265D" : "\u2657", rowCount * 100 + 110, colCount * 100 + 180);
+                } else if (board[rowCount][colCount].getPiece() == 4 || board[rowCount][colCount].getPiece() == 10) {
+                    g.drawString((black) ? "\u265C" : "\u2656", rowCount * 100 + 110, colCount * 100 + 180);
+                } else if (board[rowCount][colCount].getPiece() == 5 || board[rowCount][colCount].getPiece() == 11) {
+                    g.drawString((black) ? "\u265B" : "\u2655", rowCount * 100 + 110, colCount * 100 + 180);
+                } else if (board[rowCount][colCount].getPiece() == 6 || board[rowCount][colCount].getPiece() == 12) {
+                    g.drawString((black) ? "\u265A" : "\u2654", rowCount * 100 + 110, colCount * 100 + 180);
+                }
+
+                if (board[rowCount][colCount].getHighlighted()) {
+                    g.setColor(Color.white);
+                    g.setFont(new Font(g.getFont().toString(), Font.PLAIN, 40));
+                    g.drawString("\u2726", rowCount * 100 + 133, colCount * 100 + 163);
                 }
 
             }

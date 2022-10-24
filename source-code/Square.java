3771a44141b68827b2cf0de35d59d62306916e3f
George Skinner <56996684+GJSkinner@users.noreply.github.com>
2022-10-24 18:11:07 +0100

    removed redundant code

--- source-code/Square.java
+++ source-code/Square.java
@@ -3,24 +3,13 @@ public class Square {
     private boolean isSelected;
     private boolean isHighlighted;
     private int piece;
-    private int isWhite;
     private boolean isPassable;
-
-    public Square() {
-        isSelected = false;
-        isHighlighted = false;
-        piece = 0;
-        isWhite = 2;
-        isPassable = false;
-    }
 
     public void setPiece(int piece) {this.piece = piece;}
 
     public void setSelected(boolean selected) {this.isSelected = selected;}
 
     public void setHighlighted(boolean highlighted) {isHighlighted = highlighted;}
-
-    public void setIsWhite(int isWhite) {this.isWhite = isWhite;}
 
     public void setPassable(boolean passable) {isPassable = passable;}
 
@@ -30,7 +19,6 @@ public class Square {
 
     public boolean getHighlighted() {return isHighlighted;}
 
-    public int getIsWhite() {return isWhite;}
-
     public boolean getPassable() {return isPassable;}
+
 }

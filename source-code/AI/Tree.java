87e6ce27cd6f56d94156689ea35a3f4aba90636b
George Skinner <56996684+GJSkinner@users.noreply.github.com>
2022-10-29 21:43:31 +0100

    Update Tree.java

--- source-code/AI/Tree.java
+++ source-code/AI/Tree.java
@@ -1,95 +1,102 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Tree {
 
     StaticValue staticValue = new StaticValue();
 
     private RuleBook ruleBook;
-    private Square[][] board;
-    private ArrayList<Node> nodes;
-    private int depth = 2;
-    private int branchingFactor = 1;
-    private boolean isWhiteTurn;
+    private int[][] bestBoard;
 
-    public void constructTree() {
+    public int minimax(int[][] board, int depth, int alpha, int beta, boolean isWhiteTurn) {
 
-        nodes = new ArrayList<>();
-        nodes.add(constructNode(board, new Node(), new int[4]));
-        int parentNodeIndex = 0;
-        depth--;
+        if (depth == 0) return staticValue.getStaticValue(board);
 
-        while (depth > 0) {
-            while (branchingFactor > 0) {
+        ArrayList<int[][]> children = getChildren(board, isWhiteTurn);
 
-                Node parentNode = nodes.get(parentNodeIndex);
-                int branches = 0;
+        if (isWhiteTurn) {
 
-                for (int x = 0; x < board.length; x++) {                                                                      // for every piece on the board
-                    for (int y = 0; y < board[0].length; y++) {
+            int maxEval = -1000;
 
-                        if (isCurrPlayersPiece(board, x, y)) {                                                                 // if that piece belongs to the current player
+            for (int[][] child : children) {
 
-                            ruleBook.setBoard(board);                                                                         // find all the moves that piece can currently make
-                            ruleBook.findMoves(x, y, isWhiteTurn);
-                            ArrayList<Integer> highlights = ruleBook.highlightSquares(x, y);
-
-                            for (int i = 0; i < highlights.size(); i += 2) {                                                  // simulate that move
-
-                                int tempPiece = board[highlights.get(i)][highlights.get(i + 1)].getPiece();
-                                board[highlights.get(i)][highlights.get(i + 1)].setPiece(board[x][y].getPiece());
-                                board[x][y].setPiece(0);
-
-                                int[] move = {x, y, highlights.get(i), highlights.get(i + 1)};
-                                nodes.add(constructNode(board, parentNode, move));                                                  // and save the result in the form of a node
-                                branches++;
-
-                                board[x][y].setPiece(board[highlights.get(i)][highlights.get(i + 1)].getPiece());
-                                board[highlights.get(i)][highlights.get(i + 1)].setPiece(tempPiece);
-
-                            }
-
-                        }
-
-                    }
-                }
-
-                parentNode.setBranches(branches);
-                parentNodeIndex++;
-                branchingFactor--;
+                int evalBoard = minimax(child, depth - 1, alpha, beta, false);
+                maxEval =  Math.max(maxEval, evalBoard);
+                alpha = Math.max(alpha, evalBoard);
+                if (beta <= alpha) break;
 
             }
 
-            branchingFactor = nodes.get(parentNodeIndex).getBranches();
-            isWhiteTurn = !isWhiteTurn;
-            depth--;
+            return maxEval;
+
+        } else {
+
+            int minEval = 1000;
+            int[][] minEvalBoard = new int[8][8];
+
+            for (int[][] child : children) {
+
+                int evalBoard = minimax(child, depth - 1, alpha, beta, true);
+
+                if (evalBoard < minEval) {
+                    minEval = evalBoard;
+                    minEvalBoard = child.clone();
+                }
+
+                beta = Math.min(beta, evalBoard);
+                if (beta <= alpha) break;
+
+            }
+
+            bestBoard = minEvalBoard;
+            return minEval;
 
         }
 
     }
 
-    public Node constructNode(Square[][] board, Node parentNode, int[] move) {
+    public ArrayList<int[][]> getChildren(int[][] board, boolean isWhiteTurn) {
 
-        Node node = new Node();
-        node.setDepth(depth);
-        node.setBoardLayout(board);
-        staticValue.master(board);
-        node.setStaticValue(staticValue.getStaticValue());
-        node.setParent(parentNode);
-        node.setMove(move);
-        return node;
+        ArrayList<int[][]> children = new ArrayList<>();
+
+        for (int x = 0; x < board.length; x++) {
+            for (int y = 0; y < board[0].length; y++) {
+
+                if (isCurrPlayersPiece(board, isWhiteTurn, x, y)) {
+
+                    ruleBook.setBoard(board);
+                    ruleBook.findMoves(x, y, isWhiteTurn);
+                    ArrayList<Integer> highlights = ruleBook.highlightSquares(x, y);
+
+                    for (int i = 0; i < highlights.size(); i += 2) {
+
+                        int[][] newBoard = Arrays.stream(board).map(int[]::clone).toArray(int[][]::new);
+
+                        newBoard[highlights.get(i)][highlights.get(i + 1)] = newBoard[x][y];
+                        newBoard[x][y] = 0;
+
+                        children.add(newBoard);
+
+                    }
+
+                }
+
+            }
+        }
+
+        return children;
 
     }
 
-    public boolean isCurrPlayersPiece(Square[][] board, int curXPos, int curYPos) {
+    public boolean isCurrPlayersPiece(int[][] board, boolean isWhiteTurn, int curXPos, int curYPos) {
 
         int currPieces = isWhiteTurn ? 6 : 12;
-        return board[curXPos][curYPos].getPiece() <= currPieces && board[curXPos][curYPos].getPiece() > currPieces - 6;
+        return board[curXPos][curYPos] <= currPieces && board[curXPos][curYPos] > currPieces - 6;
 
     }
 
     public void setRuleBook(RuleBook ruleBook) {this.ruleBook = ruleBook;}
-    public void setBoard(Square[][] board) {this.board = board;}
 
-    public ArrayList<Node> getNodes() {return nodes;}
+    public int[][] getBestBoard() {return bestBoard;}
 
 }

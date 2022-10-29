addce87a79377df3c877fd398c8e61418b9fa2ba
George Skinner <56996684+GJSkinner@users.noreply.github.com>
2022-10-29 21:39:10 +0100

    Update ChessAlgorithm.java

--- source-code/ChessAlgorithm.java
+++ source-code/ChessAlgorithm.java
@@ -1,7 +1,8 @@
+import java.util.ArrayList;
+
 public class ChessAlgorithm {
 
     RuleBook ruleBook = new RuleBook();
-    StaticValue staticValue = new StaticValue();
     MoveGenerator moveGenerator = new MoveGenerator();
 
     private int curXPos = 100;
@@ -9,18 +10,17 @@ public class ChessAlgorithm {
     private int oldXPos = 0;
     private int oldYPos = 0;
 
-    private int turnsSince = 0;
     private boolean isWhiteTurn = true;
-    private static Square[][] board;
+    private static int[][] board;
+    private ArrayList<Integer> highlights = new ArrayList<>();
 
     public ChessAlgorithm() {
 
-        board = new Square[8][8];
+        board = new int[8][8];
 
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                board[i][j] = new Square();
-                board[i][j].setPiece(0);
+                board[i][j] = 0;
             }
         }
 
@@ -35,20 +35,20 @@ public class ChessAlgorithm {
             int y = isWhiteTurn ? 7 : 0;
 
             for (int x = 0; x < 8; x++) {
-                board[x][Math.abs(y - 1)].setPiece(1 + i);
+                board[x][Math.abs(y - 1)] = (1 + i);
             }
 
-            board[1][y].setPiece(2 + i);
-            board[6][y].setPiece(2 + i);
+            board[1][y] = (2 + i);
+            board[6][y] = (2 + i);
 
-            board[2][y].setPiece(3 + i);
-            board[5][y].setPiece(3 + i);
+            board[2][y] = (3 + i);
+            board[5][y] = (3 + i);
 
-            board[0][y].setPiece(4 + i);
-            board[7][y].setPiece(4 + i);
+            board[0][y] = (4 + i);
+            board[7][y] = (4 + i);
 
-            board[3][y].setPiece(5 + i);
-            board[4][y].setPiece(6 + i);
+            board[3][y] = (5 + i);
+            board[4][y] = (6 + i);
 
             isWhiteTurn = false;
 
@@ -62,102 +62,80 @@ public class ChessAlgorithm {
 
         isWhiteTurn = false;
 
-        board[oldXPos][oldYPos].setSelected(false);
-        unhighlightSquares();
-
-        moveGenerator.selectRandomPiece(ruleBook, board);
-
-        oldXPos = moveGenerator.getOldXPos();
-        oldYPos = moveGenerator.getOldYPos();
-        curXPos = moveGenerator.getCurXPos();
-        curYPos = moveGenerator.getCurYPos();
-
-        movePiece();
+        board = moveGenerator.minimaxTest(ruleBook, board);
+        if (board[curXPos][curYPos] == 13) board[curXPos][curYPos] = 1;
 
         isWhiteTurn = true;
 
     }
 
-    public boolean isNotCurrPlayersPiece(int curXPos, int curYPos) {
+    public boolean isCurrPlayersPiece(int curXPos, int curYPos) {
 
         int currPieces = isWhiteTurn ? 6 : 12;
-        return board[curXPos][curYPos].getPiece() > currPieces || board[curXPos][curYPos].getPiece() <= currPieces - 6;
+        return board[curXPos][curYPos] > currPieces - 6 && board[curXPos][curYPos] <= currPieces;
 
     }
 
     public void selectSquare() {
 
-        board[oldXPos][oldYPos].setSelected(false);
-
-        if (!isNotCurrPlayersPiece(curXPos, curYPos)) {
-
-            unhighlightSquares();
-            board[curXPos][curYPos].setSelected(true);
+        if (isCurrPlayersPiece(curXPos, curYPos)) {
 
             ruleBook.setBoard(board);
             ruleBook.findMoves(curXPos, curYPos, isWhiteTurn);
-            ruleBook.highlightSquares(curXPos, curYPos);
+            highlights = ruleBook.highlightSquares(curXPos, curYPos);
 
             oldXPos = curXPos;
             oldYPos = curYPos;
 
-        } else if (board[curXPos][curYPos].getHighlighted()) {
+        } else if (isHighlighted(curXPos, curYPos)) {
+
             movePiece();
             getAIMove();
-        } else {
-            unhighlightSquares();
+
+        } else {highlights.clear();}
+
+    }
+
+    public boolean isHighlighted(int x, int y) {
+
+        for (int i = 0; i < highlights.size(); i++) {
+            if (highlights.get(i) == x && highlights.get(i + 1) == y) return true;
         }
+
+        return false;
 
     }
 
     public void movePiece() {
 
-        turnsSince++;
+        board[curXPos][curYPos] = board[oldXPos][oldYPos];
+        board[oldXPos][oldYPos] = 0;
 
-        board[curXPos][curYPos].setPiece(board[oldXPos][oldYPos].getPiece());
-        board[oldXPos][oldYPos].setPiece(0);
-        unhighlightSquares();
-
-        if (board[curXPos][curYPos].getPiece() == 4 || board[curXPos][curYPos].getPiece() == 10) rookMoved();
-        if (board[curXPos][curYPos].getPiece() == 6 || board[curXPos][curYPos].getPiece() == 12) castling();  // handles castling stuff
-        if (board[curXPos][curYPos].getPiece() == 1 || board[curXPos][curYPos].getPiece() == 7) pawnFeatures(); // handles special pawn cases
-
-        if (turnsSince == 1) {  // prevents en passant after first turn
-            for (int x = 0; x < board.length; x++) {
-                for (int y = 0; y < board[0].length; y++) {
-                    board[x][y].setPassable(false);
-                }
-            }
-        }
-
-        staticValue.setWhiteTurn(isWhiteTurn);
-        staticValue.computeStaticValue(board);
-        System.out.println("static value: " + staticValue.getStaticValue());
+        if (board[curXPos][curYPos] == 4 || board[curXPos][curYPos] == 10) rookMoved();  // handles castling stuff
+        if (board[curXPos][curYPos] == 6 || board[curXPos][curYPos] == 12) castling();
+        if (board[curXPos][curYPos] == 1 || board[curXPos][curYPos] == 7) pawnFeatures(); // handles special pawn cases
 
     }
 
     public void pawnFeatures() {
 
         if (isWhiteTurn && curYPos == 0) {  // promotion to queen
-            board[curXPos][curYPos].setPiece(5);
+            board[curXPos][curYPos] = 5;
         } else if (!isWhiteTurn && curYPos == 7) {
-            board[curXPos][curYPos].setPiece(11);
+            board[curXPos][curYPos] = 11;
         }
 
         if (Math.abs(oldYPos - curYPos) > 1) {  // sets en passant as possible
-            board[curXPos][curYPos].setPassable(true);
-            turnsSince = 0;
+            board[curXPos][curYPos] = isWhiteTurn ? 13 : 14;
         }
 
         if (isWhiteTurn) {  // performs the en passant move
-            if (board[curXPos][curYPos + 1].getPassable() && turnsSince == 1 && isNotCurrPlayersPiece(curXPos, curYPos + 1)) {
-                board[curXPos][curYPos + 1].setPiece(0);
-                board[curXPos][curYPos + 1].setPassable(false);
+            if (board[curXPos][curYPos + 1] == 14) {
+                board[curXPos][curYPos + 1] = 0;
             }
         } else {
-            if (board[curXPos][curYPos - 1].getPassable() && turnsSince == 1 && isNotCurrPlayersPiece(curXPos, curYPos - 1)) {
-                board[curXPos][curYPos - 1].setPiece(0);
-                board[curXPos][curYPos - 1].setPassable(false);
+            if (board[curXPos][curYPos - 1] == 13) {
+                board[curXPos][curYPos - 1] = 0;
             }
         }
 
@@ -188,25 +166,15 @@ public class ChessAlgorithm {
         int rook = (isWhiteTurn) ? 4 : 10;
 
         if (oldXPos - curXPos == 2) { // if left castle
-            board[0][y].setPiece(0);
-            board[3][y].setPiece(rook);
+            board[0][y] = 0;
+            board[3][y] = rook;
             curXPos = 3;
             curYPos = y;
         } else if (oldXPos - curXPos == -2) { // if right castle
-            board[7][y].setPiece(0);
-            board[5][y].setPiece(rook);
+            board[7][y] = 0;
+            board[5][y] = rook;
             curXPos = 5;
             curYPos = y;
-        }
-
-    }
-
-    public void unhighlightSquares() {
-
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                board[i][j].setHighlighted(false);
-            }
         }
 
     }
@@ -214,6 +182,6 @@ public class ChessAlgorithm {
     public void setCurYPos(int curYPos) {this.curYPos = curYPos;}
     public void setCurXPos(int curXPos) {this.curXPos = curXPos;}
 
-    public static Square[][] getBoard() {return board;}
+    public static int[][] getBoard() {return board;}
 
 }
